e67e9a826613a39bef5711aad384c2915bb6f6d2
hakraj <adekunleddream@gmail.com>
2023-09-15 15:19:19 +0100

    chore: fix mongodb connection error

--- lib/mongodb/client.ts
+++ lib/mongodb/client.ts
@@ -6,20 +6,20 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI as string
-const options: MongoClientOptions = {
-  // autoIndex: false, // Don't build indexes
-  maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
-};
+// const options: MongoClientOptions = {
+//   // autoIndex: false, // Don't build indexes
+//   maxPoolSize: 10, // Maintain up to 10 socket connections
+//   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+//   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+//   family: 4 // Use IPv4, skip trying IPv6
+// };
 
 
 let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
 // In production mode, it's best to not use a global variable.
-client = new MongoClient(uri, options)
+client = new MongoClient(uri)
 clientPromise = client.connect()
 
 // Export a module-scoped MongoClient promise. By doing this in a
