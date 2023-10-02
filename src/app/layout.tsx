dde4a57bad306e20fc42e4cfb1f19d7e1f3302fd
hakraj <adekunleddream@gmail.com>
2023-10-02 21:33:58 +0100

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const openSans = Open_Sans({ subsets: ['latin'] })
 
 export const year = new Date().getFullYear();
 
-export const metadata = {
+const metadata = {
   title: 'Ihub - Your one stop for all needs',
   description: 'Your Ultimate Shopping Destination. Discover, Shop, and Experience a World of Quality Products and Unbeatable Deals, All in One Place.',
 }
