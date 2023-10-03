2737b2010d04f846ad0f5831ff43889e82b73ca6
hakraj <adekunleddream@gmail.com>
2023-10-03 02:46:40 +0100

    ...

--- src/app/components/navigation/SearchBar.tsx
+++ src/app/components/navigation/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, useState } from "react";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
 import { useRouter } from "next/navigation";
 
 
@@ -11,7 +11,8 @@ const SearchBar = () => {
     setQuery(value)
   }
 
-  const handleSubmit = () => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
     if (query === null || undefined || "") {
       return;
     }
@@ -20,14 +21,16 @@ const SearchBar = () => {
   }
 
   return (
-    <span className="relative">
-      <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} className="py-1 pl-4 pr-8 rounded-2xl font-light text-sm border border-[#D7BFDC] focus:outline-[#E4A0F7]" type="text" name="search" id="search" value={query} placeholder="type your search" />
-        <svg onClick={handleSubmit} className="absolute w-5 h-5 top-0 lg:top-1 text-[#8F00FF] hover:text-[#AF69EE] right-4 cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>
-      </form>
-    </span>
+    <form onSubmit={handleSubmit}>
+      <span className="relative">
+        <input onChange={handleChange} required className="py-1 pl-4 pr-8 rounded-2xl font-light text-sm border border-[#D7BFDC] focus:outline-[#E4A0F7]" type="text" name="search" id="search" value={query} placeholder="type your search" />
+        <button className="border-0 bg-transparent" type="submit">
+          <svg className="absolute w-5 h-5 top-0 lg:top-1 text-[#8F00FF] hover:text-[#AF69EE] right-4 cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+          </svg>
+        </button>
+      </span>
+    </form>
   )
 };
 
