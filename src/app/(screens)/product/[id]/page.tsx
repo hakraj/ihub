c408bd765a8e6316a2544675d5a8bfe1f9254077
hakraj <adekunleddream@gmail.com>
2023-10-03 10:21:36 +0100

    ...

--- src/app/(screens)/product/[id]/page.tsx
+++ src/app/(screens)/product/[id]/page.tsx
@@ -52,12 +52,16 @@ const ProductPage = ({ params }: { params: { id: string } }) => {
                       </svg>
                     </div>
                   </Link>
-                  <div className="w-8 h-8 rounded-[100%] flex items-center justify-center bg-black/75 hover:bg-black/50">
+                  <Link href="/checkout">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-white w-5 h-5">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                    </svg>
-                  </div>
+                    <div className="w-8 h-8 rounded-[100%] flex items-center justify-center bg-black/75 hover:bg-black/50">
+
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-white w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                      </svg>
+                    </div>
+                  </Link>
+
                 </div>
               </div>
             </div>
@@ -121,7 +125,7 @@ const ProductPage = ({ params }: { params: { id: string } }) => {
                     }
                   }}
                   className="w-8 h-8 rounded-lg border border-[#D7BFDC] text-xl text-[#D7BFDC]">-</button>
-                <p className=" font-medium">5</p>
+                <p className=" font-medium">{quantity}</p>
                 <button onClick={() => { setQuantity(prev => prev++) }} className="w-8 h-8 rounded-lg border border-[#8F00FF] text-xl text-[#8F00FF]">+</button>
               </div>
               <div className="w-2/3">
