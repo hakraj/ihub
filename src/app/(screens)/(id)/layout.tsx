e25d1c887a6b51ace19565e15040526cc17fb954
hakraj <adekunleddream@gmail.com>
2023-10-09 00:34:19 +0100

    ...

--- src/app/(screens)/(id)/layout.tsx
+++ src/app/(screens)/(id)/layout.tsx
@@ -1,24 +1,25 @@
 import Footer from "@/app/components/Footer";
 import NavBar from "@/app/components/navigation/NavigationBar";
-import styles from "../../styles.module.css"
+import styles from "../../styles.module.css";
+import IsAuth from "../../isAuth";
 
 
-export default function AuthLayout({
+export default function IdLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <main>
-      <div className={`${styles.hidelg}`}>
-        <NavBar />
-      </div>
-      {children}
-      <div className={`${styles.hidelg}`}>
-        <Footer />
-      </div>
-    </main>
-
-
+    <IsAuth>
+      <main>
+        <div className={`m-0 p-0 ${styles.hidelg}`}>
+          <NavBar />
+        </div>
+        {children}
+        <div className={`m-0 p-0 ${styles.hidelg}`}>
+          <Footer />
+        </div>
+      </main>
+    </IsAuth>
   )
 }
