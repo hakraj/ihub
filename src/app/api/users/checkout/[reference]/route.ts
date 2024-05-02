b0d4c43e1690b434274a212c4391415e9b3e4a22
hakraj <adekunleddream@gmail.com>
2024-05-02 16:28:03 +0100

    ...

--- src/app/api/users/checkout/[reference]/route.ts
+++ src/app/api/users/checkout/[reference]/route.ts
@@ -3,11 +3,13 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req: Request, { params }: { params: { reference: string } }) {
   try {
+    console.log(process.env.PAYSTACK_SECRET_KEY);
+
     const res = await fetch(`https://api.paystack.co/transaction/verify/${params.reference}`,
       {
         method: "GET",
         headers: {
-          authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
+          'Authorization': `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
           // Add other headers if needed
         }
       })
